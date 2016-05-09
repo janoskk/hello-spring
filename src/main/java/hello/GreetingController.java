2cd3d57da6dc989822d1ce9404fb95e3fe4deb4e
Janos Kasza <bipketto@gmail.com>
2016-05-09 20:44:13 +0200

    Mongo access provided

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,9 +1,11 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
@@ -11,9 +13,18 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private PersonRepository repository;
+
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+
+        String names = name;
+        for (Person person : repository.findAll()) {
+            names += ", " + person.getFirstName();
+        }
+
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                            String.format(template, names));
     }
 }
--- src/main/java/hello/SecurityConfig.java
+++ src/main/java/hello/SecurityConfig.java
@@ -2,15 +2,23 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("user").password("pass").roles("USER").and()
+                .withUser("admin").password("pass").roles("USER", "ADMIN");
     }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().fullyAuthenticated();
+//        http.httpBasic();
+//        http.csrf().disable();
+//    }
 }
